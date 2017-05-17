6a61907d38f041a3b5d41fa77791cf5c55fe00b9
lianshuideyu <834893479@qq.com>
2017-05-17 20:10:54 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/myandroidgittest222/MainActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.myandroidgittest222;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("版本1.0");
+    }
+}
